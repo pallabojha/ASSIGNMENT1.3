1802d8ec7376e9ab7c1058d4fa2fed348fe99f34
pallabojha <ojhapallab@gmail.com>
2017-03-15 16:07:27 +0530

    SWAPS
    SWAPS

---
+++ src/assignment1/pkg31/Assignment131.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1.pkg31;
+
+/**
+ *
+ * @author IIS-KOLKATA
+ */
+public class Assignment131 {
+
+    
+    public static void main(String[] args) {
+     int a,b;
+		a =10;
+		b= 12;
+		System.out.println("Before Swaps :");
+		System.out.println("a :"+a);
+		System.out.println("b :"+b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("After Swaps :");
+		System.out.println("a :"+a);
+		System.out.println("b :"+b);
+	}
+
+    }
+    
+
